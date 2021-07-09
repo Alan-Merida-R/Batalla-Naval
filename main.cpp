b6fd892b9cd42b9c7a15acbf14456294b43a50a0
Alan-Merida-R <87174402+Alan-Merida-R@users.noreply.github.com>
2021-07-09 04:53:39 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//Funcion principal
+#include<stdio.h>
+#include<string.h>
+#include <stdlib.h>
+
+int main()
+{
+	printf("");
+	gets(Variable);
+	//fgets(variable, 2000, stdin);
+	//scanf("%",&);
+	setbuf(stdin, NULL);
+	fflush(stdin);
+	return 0;
+}
