8082db58fee1ea655bb2f4cfdd3da0e73b80837f
Alan-Merida-R <87174402+Alan-Merida-R@users.noreply.github.com>
2021-07-09 05:12:48 -0500

    Create Estructura.cpp

---
+++ Estructura.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+#include <stdlib.h>
+int main()
+{
+	printf("");
+	gets(Variable);
+	//fgets(variable, 2000, stdin);
+	//scanf("%",&);
+	setbuf(stdin, NULL);
+	fflush(stdin);
+	return 0;
+}
