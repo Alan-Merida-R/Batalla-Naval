a8ec22915c253d5d877d7aa1e2477f37bfb28db2
Alan-Merida-R <87174402+Alan-Merida-R@users.noreply.github.com>
2021-07-09 05:53:43 -0500

    Update Estructura.cpp

--- Estructura.cpp
+++ Estructura.cpp
@@ -1,13 +1,41 @@
 #include<stdio.h>
 #include<string.h>
 #include <stdlib.h>
-int main()
-{
-	printf("");
-	gets(Variable);
-	//fgets(variable, 2000, stdin);
-	//scanf("%",&);
-	setbuf(stdin, NULL);
-	fflush(stdin);
-	return 0;
-}
+//Lista doblemente ligada
+struct Caracteristicas{
+	char *nombreDelTablero;
+	int posicion[5][5];//[x][y]
+	int nivel;
+};
+struct Tableros{
+	Caracteristicas *nueva;
+	Tableros *siguiente;
+ 	Tableros *anterior;
+}*primero=NULL,*ultimo=NULL;
+struct Datos{
+	int id;
+	char *usuario;
+	char *contra;
+	int puntos;
+	int vidas;
+	int rectagular;
+	int cuadrada;
+	int atomica;
+	Tableros *tablerosDeUsuario;
+};
+struct Usuarios{
+	Datos *ingreso;
+	Usuarios *siguiente;
+	Usuarios *anterior;
+}*inicio=NULL,*fin=NULL;
+ void ingresarUsuario();//Funciones para usuarios
+ void buscarUsuarios();
+ void modificarUsuarios();
+ void mostrarUsuarios();
+ 
+ void ingresarTablero();//Funciones para el tablero
+ void buscarTablero();
+ void modificarTablero();
+ void eliminarTablero();
+ void mostrarTableros();
+ 
