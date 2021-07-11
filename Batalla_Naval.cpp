c28d104ab30be4e0931225466e8e87eb1e9fee34
Diego Badillo <diegobadillo@live.com>
2021-07-11 15:19:51 -0500

    Agregados nuevos Arte ASCII

--- Batalla_Naval.cpp
+++ Batalla_Naval.cpp
@@ -4,18 +4,19 @@
 #include <stdlib.h> // Librería para la función system("clear")
 #include <unistd.h> // Librería para la función sleep()
 #include <windows.h>//Libreria para el color
-void login();
-void aniInicial();
-void aniAhorcado();
-void AltEnter();
+void login(); //Pide datos
+void aniLogo(); //Letrotas
+void aniBarco(); //Imagen ASCII de un barco (Voy a ver si logro hacer que parpadee
+void aniAvion(); // " 				" avion
+void barraSup();
 
-int main(){
-	login();
+int main(){ //Main para probar las animaciones BORRAR DESPUES 
+	aniLogo();
 }
 
 void login(){
 char user[10], pass[10];
-	/*
+	
 	system("cls");
 	printf("\033[0;31m"); 	//Iniciamos color ROJO
 	printf("\t\t\t\t\t%c",201);  for(int p=0; p<24; p++){printf("%c",205);} printf("%c\n",187); printf("\t\t\t\t\t%c \t",186); //Parte de arriba del recuadro
@@ -52,7 +53,7 @@ char user[10], pass[10];
 	system("CLS");
 	//Aqui metan que se verifique que pass y user no sea el mismo 
 	//y poner una condicion para que se ingresen el valor 
-	*/
+	
 	//alerta de datos incorectos
 	system("cls");
 	printf("\033[0;31m"); 	//Iniciamos color ROJO
@@ -61,82 +62,65 @@ char user[10], pass[10];
 	printf("\t\t\t\t\t%c",200);  for(int p=0; p<31; p++){printf("%c",205);} printf("%c\n",188);	 //Parte de abajo del recuadro 
 	printf("\033[0m");
 
-	/*
+	
 	//Si todo es correcto
 	printf("\n\t\t\t\t\tPerfecto! Es hora de empezar el juego!!\n");		
 	sleep(3);
 	system("CLS");
-	*/
-}
-
-void aniInicial(){
 	
-	system("CLS"); // Para limpiar la pantalla despues de cada interacción con el juego
-    printf("\033[5;3;1;35m"); // Códigos para darle formato, colores y animaciones a lo mostrado en pantalla
-    // Pantalla de inicio
-    printf("\n\n\n\n\n\t\t\t     A BADILLO'S PRODUCTION\n\n\n\n\t\t\t       POWERED BY ALAN\n\n\n\n\t\t\t\t ©2020 ENRIQUE-PINEIRO");
-    
-	printf("\n\n\n\n\t\t\t  PRESIONA ENTER PARA CONTINUAR...");
-    printf("\033[0m"); // Códigos para restablecer el formato inicial, sino el resto del programa adopta el mismo formato declarado arriba
-    getchar(); // Función para darle una pausa al programa hasta que el usuario presione Enter
-	system("CLS");
-	printf("BIENVENIDO A \n");
+}
 
+void aniLogo(){
+	system("cls");
+printf("\033[0;32m");                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+printf(":::::::::      ::: ::::::::::: :::     :::        :::            :::   					\n");              
+printf(":+:    :+:   :+: :+:   :+:   :+: :+:   :+:        :+:          :+: :+:               	\n");
+printf("+:+    +:+  +:+   +:+  +:+  +:+   +:+  +:+        +:+         +:+   +:+              	\n");
+printf("+#++:++#+  +#++:++#++: +#+ +#++:++#++: +#+        +#+        +#++:++#++:             	\n");
+printf("+#+    +#+ +#+     +#+ +#+ +#+     +#+ +#+        +#+        +#+     +#+             	\n");
+printf("#+#    #+# #+#     #+# #+# #+#     #+# #+#        #+#        #+#     #+#             	\n");
+printf("#########  ###     ### ### ###     ### ########## ########## ###     ###             	\n");
+printf("                          ::::    :::     :::     :::     :::     :::     :::        	\n");
+printf("                          :+:+:   :+:   :+: :+:   :+:     :+:   :+: :+:   :+:        	\n");
+printf("                          :+:+:+  +:+  +:+   +:+  +:+     +:+  +:+   +:+  +:+        	\n");
+printf("                          +#+ +:+ +#+ +#++:++#++: +#+     +:+ +#++:++#++: +#+        	\n");
+printf("                          +#+  +#+#+# +#+     +#+  +#+   +#+  +#+     +#+ +#+        	\n");
+printf("                          #+#   #+#+# #+#     #+#   #+#+#+#   #+#     #+# #+#        	\n");
+printf("                          ###    #### ###     ###     ###     ###     ### ########## 	\n");
 
-                                                                                                                                                                                                                                 
-                                                                                                                                                                                                                                 
-printf("BBBBBBBBBBBBBBBBB                             tttt                            lllllll lllllll                      \n"); 
-printf("B::::::::::::::::B                         ttt:::t                            l:::::l l:::::l                      \n");
-printf("B::::::BBBBBB:::::B                        t:::::t                            l:::::l l:::::l                      \n");
-printf("BB:::::B     B:::::B                       t:::::t                            l:::::l l:::::l                      \n");
-printf("  B::::B     B:::::B  aaaaaaaaaaaaa  ttttttt:::::ttttttt      aaaaaaaaaaaaa    l::::l  l::::l   aaaaaaaaaaaaa      \n");
-printf("  B::::B     B:::::B  a::::::::::::a t:::::::::::::::::t      a::::::::::::a   l::::l  l::::l   a::::::::::::a     \n");
-printf("  B::::BBBBBB:::::B   aaaaaaaaa:::::at:::::::::::::::::t      aaaaaaaaa:::::a  l::::l  l::::l   aaaaaaaaa:::::a    \n");
-printf("  B:::::::::::::BB             a::::atttttt:::::::tttttt               a::::a  l::::l  l::::l            a::::a    \n");
-printf("  B::::BBBBBB:::::B     aaaaaaa:::::a      t:::::t              aaaaaaa:::::a  l::::l  l::::l     aaaaaaa:::::a    \n");
-printf("  B::::B     B:::::B  aa::::::::::::a      t:::::t            aa::::::::::::a  l::::l  l::::l   aa::::::::::::a    \n");
-printf("  B::::B     B:::::B a::::aaaa::::::a      t:::::t           a::::aaaa::::::a  l::::l  l::::l  a::::aaaa::::::a    \n");
-printf("  B::::B     B:::::Ba::::a    a:::::a      t:::::t    tttttta::::a    a:::::a  l::::l  l::::l a::::a    a:::::a    \n");
-printf("BB:::::BBBBBB::::::Ba::::a    a:::::a      t::::::tttt:::::ta::::a    a:::::a l::::::ll::::::la::::a    a:::::a    \n");
-printf("B:::::::::::::::::B a:::::aaaa::::::a      tt::::::::::::::ta:::::aaaa::::::a l::::::ll::::::la:::::aaaa::::::a    \n");
-printf("B::::::::::::::::B   a::::::::::aa:::a       tt:::::::::::tt a::::::::::aa:::al::::::ll::::::l a::::::::::aa:::a   \n");
-printf("BBBBBBBBBBBBBBBBB     aaaaaaaaaa  aaaa         ttttttttttt    aaaaaaaaaa  aaaallllllllllllllll  aaaaaaaaaa  aaaa   \n");
-                                                                                                                                                                                                                                 
-printf("\n \n");                                                                                        
                                                                                         
-printf("NNNNNNNN        NNNNNNNN                                                        lllllll  \n");
-printf("N:::::::N       N::::::N                                                        l:::::l  \n");
-printf("N::::::::N      N::::::N                                                        l:::::l  \n");
-printf("N:::::::::N     N::::::N                                                        l:::::l  \n");
-printf("N::::::::::N    N::::::N  aaaaaaaaaaaaavvvvvvv           vvvvvvvaaaaaaaaaaaaa    l::::l  \n");
-printf("N:::::::::::N   N::::::N  a::::::::::::av:::::v         v:::::v a::::::::::::a   l::::l  \n");
-printf("N:::::::N::::N  N::::::N  aaaaaaaaa:::::av:::::v       v:::::v  aaaaaaaaa:::::a  l::::l  \n");
-printf("N::::::N N::::N N::::::N           a::::a v:::::v     v:::::v            a::::a  l::::l  \n");
-printf("N::::::N  N::::N:::::::N    aaaaaaa:::::a  v:::::v   v:::::v      aaaaaaa:::::a  l::::l  \n");
-printf("N::::::N   N:::::::::::N  aa::::::::::::a   v:::::v v:::::v     aa::::::::::::a  l::::l  \n");
-printf("N::::::N    N::::::::::N a::::aaaa::::::a    v:::::v:::::v     a::::aaaa::::::a  l::::l  \n");
-printf("N::::::N     N:::::::::Na::::a    a:::::a     v:::::::::v     a::::a    a:::::a  l::::l  \n");
-printf("N::::::N      N::::::::Na::::a    a:::::a      v:::::::v      a::::a    a:::::a l::::::l \n");
-printf("N::::::N       N:::::::Na:::::aaaa::::::a       v:::::v       a:::::aaaa::::::a l::::::l \n");
-printf("N::::::N        N::::::N a::::::::::aa:::a       v:::v         a::::::::::aa:::al::::::l \n");
-printf("NNNNNNNN         NNNNNNN  aaaaaaaaaa  aaaa        vvv           aaaaaaaaaa  aaaallllllll \n");
-                                                                                        
-                                                                                        
-                                                                                        
-                                                                                        
-                                                                                        
-                                                                                        
-                                                                                        
-                                                                                                                                                                                                                                 
-                                                                                                                                                                                                                                 
-                                                                                                                                                                                                                                 
-                                                                                                                                                                                                                                 
-                                                                                                                                                                                                                                 
-                                                                                                                                                                                                                                 
+}                                                                       
 
+void aniBarco(){
+	
+	
+	
+printf("                                   )___(						\n");
+printf("                           _______/__/_							\n");
+printf("                  ___     /===========|   ___					\n");
+printf(" ____       __   [\\\\\\]___/____________|__[///]   __			\n");	
+printf(" \\   \\_____[\\\\]__/___________________________\\__[//]___	\n");
+printf("  \\40A                                                 |		\n");
+printf("   \\                                                  /		\n");
+printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~	\n");
+	
+}
 
+void aniAvion(){
+	
+printf("    	   __					\n");
+printf("           \\  \\     _ _		\n");
+printf("            \\**\\ ___\\/ \\	\n");
+printf("          X*#####*+^^\\_\\		\n");
+printf("            o/\\  \\			\n");
+printf("               \\__\\			\n");
 
 }
 
- 
+void barraSup(){
+	
+	
+	
+}
+
 
