110d6c06c0472184b8c742ba730d02632290d13c
Alan-Merida-R <87174402+Alan-Merida-R@users.noreply.github.com>
2021-07-16 04:46:22 -0500

    Update MainPrincipal.cpp

--- MainPrincipal.cpp
+++ MainPrincipal.cpp
@@ -9,9 +9,58 @@
 #define falso 0
 //Programas
 #include"FuncionesEstructurales.cpp"
-usuarios **bajarDatosAListaCircular();
 int main()
 {
+	//Inicio del programa para bajar todos los datos del usuario
+	usuarios **actualPrincipal=(usuarios**)malloc(sizeof(usuarios*));
+	usuarios *primeroPrincipal=(usuarios*)malloc(sizeof(usuarios));
+ 	usuarios *ultimoPrincipal=(usuarios*)malloc(sizeof(usuarios));
+ 	
+	char *usuarioBajar=(char*)malloc(sizeof(char));
+	char *contraBajar=(char*)malloc(sizeof(char));
+	char *idLetra=(char*)malloc(sizeof(char));
+	char *puntosLetra=(char*)malloc(sizeof(char));
+	char *vidasLetra=(char*)malloc(sizeof(char));
+	char *rectangularLetra=(char*)malloc(sizeof(char));
+	char *cuadradaLetra=(char*)malloc(sizeof(char));
+	char *atomicaLetra=(char*)malloc(sizeof(char));
+
+	int idBajar,puntosBajar,vidasBajar,rectangularBajar,cuadradaBajar,atomicaBajar;
+	int x=0;
+
+	x=CantidadDeRenglones(ArchivoUsuarios);
+	for(int i=1;i<x;i++){
+		if(i==1){
+			primeroPrincipal=NULL;
+			ultimoPrincipal=NULL;
+			actualPrincipal=NULL;
+		}
+		idLetra=LeerArchivo(ArchivoUsuarios,i,1);
+		usuarioBajar=LeerArchivo(ArchivoUsuarios,i,2);
+		contraBajar=LeerArchivo(ArchivoUsuarios,i,3);
+		puntosLetra=LeerArchivo(ArchivoUsuarios,i,4);
+		vidasLetra=LeerArchivo(ArchivoUsuarios,i,5);
+		rectangularLetra=LeerArchivo(ArchivoUsuarios,i,6);
+		cuadradaLetra=LeerArchivo(ArchivoUsuarios,i,7);
+		atomicaLetra=LeerArchivo(ArchivoUsuarios,i,8);
+
+
+		idBajar=atoi(idLetra);
+		puntosBajar=atoi(puntosLetra);
+		vidasBajar=atoi(vidasLetra);
+		rectangularBajar=atoi(rectangularLetra);
+		cuadradaBajar=atoi(cuadradaLetra);
+		atomicaBajar=atoi(atomicaLetra);
+		
+		actualPrincipal=ingresarUsuario(primeroPrincipal,ultimoPrincipal,idBajar,usuarioBajar,contraBajar,puntosBajar,vidasBajar,rectangularBajar,cuadradaBajar,atomicaBajar);
+   		primeroPrincipal=actualPrincipal[0];
+   		ultimoPrincipal=actualPrincipal[1];
+	}
+	
 	//poner un switch y mandar a llamar a todas las funciones
+	
+	
+	//final subir los cambios de la lista
+	enviarNuevaLista(primeroPrincipal);
 	return 0;
 }
